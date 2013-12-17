c8668f1ef6c143e62c0e18345dc93f49dcb6fa9e
Bob Henkel <bob.henkel@gmail.com>
2013-12-17 09:54:43 -0600

    Added template headers. These still need to be filled out correctly.

--- functions/plog.insert_debug(text,integer).sql
+++ functions/plog.insert_debug(text,integer).sql
@@ -1,6 +1,22 @@
--- Function: plog.insert_debug(text, integer)
-
--- DROP FUNCTION plog.insert_debug(text, integer);
+-- *********************************************************************************************
+-- Description: -- Opens up a new transactions by creating a new connection
+-- with dblink and commits to the log table the desired text to logging table regardless of parent 
+-- transaction being rolled back.  Primarly for instrumenting code and in 
+-- special cases doing batch commits for a large volume of data.
+--
+-- Input Parameters: log_text TEXT - The text you want to log to the logging table.
+--
+-- Output Parameters:  NA
+--
+-- Error Conditions Raised: NA
+--
+-- Author:      Bob Henkel bob.henkel@gmail.com
+--
+-- Revision History
+-- Date            Author       Reason for Change
+-- ----------------------------------------------------------------
+-- 13-DEC-2013     Bob Henkel   Initial Version
+-- *********************************************************************************************
 
 CREATE OR REPLACE FUNCTION plog.insert_debug(log_text text, p_pid integer)
   RETURNS void AS
